20832ecbf7c82f22ff83585c24272938853008fa
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-08-22 11:44:02 +0600

    Cleaning UP

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\DefaultController;
-use Illuminate\Support\Facades\Route;
-
-
-Route::get('/', [DefaultController::class, 'index'])->name('index');
-Route::get('/product', [DefaultController::class, 'product'])->name('product');
