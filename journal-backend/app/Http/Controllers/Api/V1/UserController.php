11f1e51241609bc024c9c706b671595c62cdde48
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-08-26 16:51:10 +0600

    Paginate the users

--- journal-backend/app/Http/Controllers/Api/V1/UserController.php
+++ journal-backend/app/Http/Controllers/Api/V1/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        return new UserCollection(User::all());
+        return new UserCollection(User::paginate());
     }
 
     public function show(User $user)
