9cb59abf5d01f9622a26b3e4f8a5f93ab42b73e6
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-05 22:53:01 +0600

    Fixing a major issue of the layout

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,31 +1,22 @@
 <template>
-  <!-- <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view /> -->
-  <p></p>
+  <div class="container-scroller">
+    <Sidebar />
+    <div class="container-fluid page-body-wrapper">
+      <Navbar />
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Sidebar from '@/components/Sidebar.vue'
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'Appbar',
+  components: {
+    Sidebar,
+    Navbar,
+  }
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
