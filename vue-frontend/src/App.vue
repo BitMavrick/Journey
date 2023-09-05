90ccc4eacba41b1a9623ba22e9ed6ce272c633d5
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-05 21:59:04 +0600

    Linking up success

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view /> -->
+  <p></p>
 </template>
 
 <style>
