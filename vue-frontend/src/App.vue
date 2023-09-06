8b66ea8cf6cdad6727025c23fd147a6fde016d6f
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-06 19:42:00 +0600

    Progressing ...

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container-scroller">
-    <Sidebar />
-    <div class="container-fluid page-body-wrapper">
-      <Navbar />
+    <Sidebar v-if="showSidebar" />
+    <div class="container-fluid page-body-wrapper" :class="[{ 'full-page-wrapper': showAdditionalClass }]">
+      <Navbar v-if="showSidebar" />
       <div class="main-panel">
         <router-view />
       </div>
@@ -15,10 +15,24 @@ import Sidebar from '@/components/Sidebar.vue'
 import Navbar from '@/components/Navbar.vue'
 
 export default {
-  name: 'Appbar',
+  name: 'App',
   components: {
     Sidebar,
     Navbar,
+  },
+  computed: {
+    showSidebar() {
+      // Determine whether to show the sidebar based on the current route
+      return this.$route.name !== 'login' && this.$route.name !== 'signup';
+    },
+    showNavbar() {
+      // Determine whether to show the navbar based on the current route
+      return this.$route.name !== 'login' && this.$route.name !== 'signup';
+    },
+    showAdditionalClass() {
+      // Determine whether to add the additional class based on the current route
+      return this.$route.name === 'login' || this.$route.name === 'signup';
+    },
   }
 }
 
---
+++ vue-frontend/src/views/SignupView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>This is the signup page</h1>
+</template>
