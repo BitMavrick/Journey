452df037e5217988c6095e6c2f3cc00a1faa5699
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-06 22:19:04 +0600

    Progressing ...

--- vue-frontend/src/views/SignupView.vue
+++ vue-frontend/src/views/SignupView.vue
@@ -8,7 +8,7 @@
                             <h3 class="card-title text-left mb-3">Signup</h3>
                             <form>
                                 <div class="form-group">
-                                    <label>Username</label>
+                                    <label>Name</label>
                                     <input type="text" class="form-control p_input">
                                 </div>
                                 <div class="form-group">
@@ -19,33 +19,27 @@
                                     <label>Password</label>
                                     <input type="password" class="form-control p_input">
                                 </div>
-                                <div class="form-group d-flex align-items-center justify-content-between">
-                                    <div class="form-check">
-                                        <label class="form-check-label">
-                                            <input type="checkbox" class="form-check-input"> Remember me </label>
-                                    </div>
-                                    <a href="#" class="forgot-pass">Forgot password</a>
+
+                                <div class="form-group">
+                                    <label>Re-enter Password</label>
+                                    <input type="password" class="form-control p_input">
                                 </div>
+
+                                <br>
+
                                 <div class="text-center">
-                                    <button type="submit" class="btn btn-primary btn-block enter-btn">Login</button>
+                                    <button type="submit" class="btn btn-primary btn-block enter-btn">Create
+                                        Account</button>
                                 </div>
-                                <div class="d-flex">
-                                    <button class="btn btn-facebook col mr-2">
-                                        <i class="mdi mdi-facebook"></i> Facebook </button>
-                                    <button class="btn btn-google col">
-                                        <i class="mdi mdi-google-plus"></i> Google plus </button>
-                                </div>
-                                <p class="sign-up text-center">Already have an Account?<a href="#"> Sign Up</a></p>
+                                <p class="sign-up text-center">Already have an Account?<router-link to="/login"> Log
+                                        In</router-link></p>
                                 <p class="terms">By creating an account you are accepting our<a href="#"> Terms &
                                         Conditions</a></p>
                             </form>
                         </div>
                     </div>
                 </div>
-                <!-- content-wrapper ends -->
             </div>
-            <!-- row ends -->
         </div>
-        <!-- page-body-wrapper ends -->
     </div>
 </template>
