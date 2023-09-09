1086a7f3364edb40c4c084d2a2a61bb500a77663
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-09 13:50:39 +0600

    Fixed some errors and progressing

--- vue-frontend/src/components/Sidebar.vue
+++ vue-frontend/src/components/Sidebar.vue
@@ -20,20 +20,32 @@
             <li class="nav-item nav-category">
                 <span class="nav-link">Navigation</span>
             </li>
-            <li class="nav-item menu-items">
-                <a class="nav-link" href="index.html">
+
+            <li class="nav-item active">
+                <a class="nav-link" href="#">
                     <span class="menu-icon">
-                        <i class="mdi mdi-speedometer"></i>
+                        <i class="mdi mdi-weather-sunny"></i>
                     </span>
                     <span class="menu-title">Your Day</span>
                 </a>
             </li>
+
+            <li class="nav-item menu-items">
+                <a class="nav-link"
+                    href="http://www.bootstrapdash.com/demo/corona-free/jquery/documentation/documentation.html">
+                    <span class="menu-icon">
+                        <i class="mdi mdi-file-document-box"></i>
+                    </span>
+                    <span class="menu-title">Your Notes</span>
+                </a>
+            </li>
+
             <li class="nav-item menu-items">
                 <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
                     <span class="menu-icon">
                         <i class="mdi mdi-laptop"></i>
                     </span>
-                    <span class="menu-title">Basic UI Elements</span>
+                    <span class="menu-title">Admin Controls</span>
                     <i class="menu-arrow"></i>
                 </a>
                 <div class="collapse" id="ui-basic">
@@ -44,65 +56,6 @@
                         </li>
                     </ul>
                 </div>
-            </li>
-            <li class="nav-item menu-items">
-                <a class="nav-link" href="pages/forms/basic_elements.html">
-                    <span class="menu-icon">
-                        <i class="mdi mdi-playlist-play"></i>
-                    </span>
-                    <span class="menu-title">Form Elements</span>
-                </a>
-            </li>
-            <li class="nav-item menu-items">
-                <a class="nav-link" href="pages/tables/basic-table.html">
-                    <span class="menu-icon">
-                        <i class="mdi mdi-table-large"></i>
-                    </span>
-                    <span class="menu-title">Tables</span>
-                </a>
-            </li>
-            <li class="nav-item menu-items">
-                <a class="nav-link" href="pages/charts/chartjs.html">
-                    <span class="menu-icon">
-                        <i class="mdi mdi-chart-bar"></i>
-                    </span>
-                    <span class="menu-title">Charts</span>
-                </a>
-            </li>
-            <li class="nav-item menu-items">
-                <a class="nav-link" href="pages/icons/mdi.html">
-                    <span class="menu-icon">
-                        <i class="mdi mdi-contacts"></i>
-                    </span>
-                    <span class="menu-title">Icons</span>
-                </a>
-            </li>
-            <li class="nav-item menu-items">
-                <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-                    <span class="menu-icon">
-                        <i class="mdi mdi-security"></i>
-                    </span>
-                    <span class="menu-title">User Pages</span>
-                    <i class="menu-arrow"></i>
-                </a>
-                <div class="collapse" id="auth">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html"> Blank Page </a></li>
-                        <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
-                        <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
-                        <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
-                        <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item menu-items">
-                <a class="nav-link"
-                    href="http://www.bootstrapdash.com/demo/corona-free/jquery/documentation/documentation.html">
-                    <span class="menu-icon">
-                        <i class="mdi mdi-file-document-box"></i>
-                    </span>
-                    <span class="menu-title">Documentation</span>
-                </a>
             </li>
         </ul>
     </nav>
