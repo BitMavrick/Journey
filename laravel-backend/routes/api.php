0f53d3ac3ba666b278c1abd43e8bb03d3780e366
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-19 18:39:20 +0600

    Completed the backend authentication system

--- laravel-backend/routes/api.php
+++ laravel-backend/routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
